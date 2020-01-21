644f8a282e09e6f842e61584ea5692fc710fdaa6
moncef <mrejebsf@enssat.fr>
2020-01-21 13:25:15 +0100

    migration database to cloud

--- src/database/database.js
+++ src/database/database.js
@@ -2,11 +2,11 @@ import Sequelize from 'sequelize';
 
 //connexion to database
 export const sequelize=new Sequelize(
-  'Moncef18',
-  'Moncef18',
-  'azerty',
+  'lpjibasl',
+  'lpjibasl',
+  '6o5Zh4iXTCgAdrpg9SGsRoYlM70c0X3o',
   {
-    host: 'localhost',
+    host: 'manny.db.elephantsql.com',
     dialect:'postgres',
     pool:{
       max:5,
