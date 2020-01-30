e64953471c9a3896aecc3b27c6353593ca2fe781
moncef08 <mrejebsf@enssat.fr>
2020-01-30 12:27:47 +0100

    local database for tests

--- src/database/database.js
+++ src/database/database.js
@@ -2,17 +2,17 @@ import Sequelize from 'sequelize';
 
 //connexion to database
 export const sequelize=new Sequelize(
-'Moncef18',
-'Moncef18',
+'postgres',
+'moncef',
 'azerty',
 
-  //
+
   // 'lpjibasl',
   // 'lpjibasl',
   // '6o5Zh4iXTCgAdrpg9SGsRoYlM70c0X3o',
   {
-    // host:'manny.db.elephantsql.com',
-    host: 'localhost',
+     host:'localhost',
+    // host: 'localhost',
     dialect:'postgres',
     pool:{
       max:1000,
